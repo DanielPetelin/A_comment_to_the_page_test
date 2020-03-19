981d40f1f31a35d00d4a8a88632991ba83cd3f99
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-19 18:17:19 +0300

    add fun clear

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 document.getElementById('print').onclick = function() {
     var yourMesseg = document.getElementById('yourText').value;
     var userName = document.getElementById('userName').value;
-    var message = yourMesseg + '<br>' + userName;
+    var message = yourMesseg + '<br>' + 'Сообщение от пользователя: ' + '<b>' + userName + '</b>';
 
-    document.getElementById('out').innerHTML = message;
+    document.getElementById('out').innerHTML =  message;
+};
+
+document.getElementById('clear').onclick = function() {
+    document.getElementById('yourText').value = '';
+    document.getElementById('userName').value = '';
 };
